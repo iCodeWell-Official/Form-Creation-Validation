8e53e54d8b66dd92cbf17c9ad5bf5b450e9c6539
Nasr <nasr.asri10@gmail.com>
2024-10-12 12:29:39 +0100

    Create HTML, CSS and JS initial code

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+})
